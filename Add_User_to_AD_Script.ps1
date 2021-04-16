f212212fc9446993e94b4a39abd8a86fdf800365
Robert Rodriguez <robert.rodriguez.jr.70@gmail.com>
2021-04-16 09:30:52 -0700

    Update Add_User_to_AD_Script.ps1

--- Add_User_to_AD_Script.ps1
+++ Add_User_to_AD_Script.ps1
@@ -1,8 +1,8 @@
-New-ADUser "first last"-Office <name> -mobilephone "xxx-xxx-xxxx"
-Get-ADUser "first last"
-Set-ADObject -Description "blah"
+#New-ADUser "first last"-Office <name> -mobilephone "xxx-xxx-xxxx"
+#Get-ADUser "first last"
+#Set-ADObject -Description "blah"
 
-$pw=Read-Host "What is the password" -AsSecureString
-Set-ADAccountpassword "first last" -NewPassword $pw
-Enable-ADAccount "first last"
+#$pw=Read-Host "What is the password" -AsSecureString
+#Set-ADAccountpassword "first last" -NewPassword $pw
+#Enable-ADAccount "first last"
 
