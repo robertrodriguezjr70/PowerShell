f35af477416422654545813dfe24b618620853c9
Robert Rodriguez <robert.rodriguez.jr.70@gmail.com>
2021-04-18 04:01:40 -0700

    Update Add_User_to_AD_Script.ps1

--- Add_User_to_AD_Script.ps1
+++ Add_User_to_AD_Script.ps1
@@ -8,11 +8,17 @@
 #-Enabled $True
 
 
-
 #Get-ADUser "first last"
 #Set-ADObject -Description "blah"
-
+       
 #$pw=Read-Host "What is the password" -AsSecureString
 #Set-ADAccountpassword "first last" -NewPassword $pw
 #Enable-ADAccount "first last"
 
+#$Firstname = Read-Host -Prompt "Please enter First Name"
+#$Lastname = Read-Host -Prompt "Please enter  Last Name"
+
+
+#New-ADUser
+#-Name "$firstname $Lastname" `
+#-GivenName $Firstname `
