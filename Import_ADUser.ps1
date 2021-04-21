26136885a08d73c1389739dc5a7112c6cab0a70e
Robert Rodriguez <robert.rodriguez.jr.70@gmail.com>
2021-04-21 02:31:28 -0700

    Update Import_ADUser.ps1

--- Import_ADUser.ps1
+++ Import_ADUser.ps1
@@ -1,3 +1,14 @@
+
+#view users from csv file
+Import-Csv "<file path>" |Out-GridView
+
+Import-Csv "<file path>" |
+            New-ADUser '
+            -enabled $True
+            -accountpassword $(convertTo-SecureString "password" -AsPlainText -Force)
+            
+
+
 #$users= Import-CSV <Path>
 
 ForEach ($user in $users)
