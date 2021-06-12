a8c3db7280aa0f7569ab6ec0d9ce97f9aec4b6c9
Robert Rodriguez <robert.rodriguez.jr.70@gmail.com>
2021-06-12 02:11:11 -0700

    Update Import_ADUser_CSV.ps1

--- Import_ADUser_CSV.ps1
+++ Import_ADUser_CSV.ps1
@@ -1,19 +1,8 @@
 
 #view users from csv file
-#Import-Csv "<file path>" |Out-GridView
+  #Import-Csv "<file path>" |Out-GridView
 
-#Import-Csv "<file path>" |
-#            New-ADUser '
-#            -accountpassword $(convertTo-SecureString "IpSecurity1!" -AsPlainText -Force) `
-#            -enabled $True
             
 # rrodriguez - The below code works to inport users, tested on 06/12/2021
-# Import-Csv "c:\Imports\Userimport.csv" |New-ADUser -accountpassword $(convertTo-SecureString "IpSecurity1!" -AsPlainText -Force)  -enabled $True           
+  # Import-Csv "c:\Imports\Userimport.csv" |New-ADUser -accountpassword $(convertTo-SecureString "IpSecurity1!" -AsPlainText -Force)  -enabled $True           
 
-
-#$users= Import-CSV <Path>
-
-ForEach ($user in $users)
-{
-  Write-Host $user $user.<property>
-}
